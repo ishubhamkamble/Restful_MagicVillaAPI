f00dc2f54a5a062e72e8939fa0467d3a1b2572e6
Shubham Kamble <shubhkam61@gmail.com>
2024-01-17 13:16:57 +0530

    Seed Villa Table data with Records

--- Restful_MagicVillaAPI/Migrations/20240117074412_SeedVillaTable.Designer.cs
+++ Restful_MagicVillaAPI/Migrations/20240117074412_SeedVillaTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restful_MagicVillaAPI.Data;
 
@@ -11,9 +12,11 @@ using Restful_MagicVillaAPI.Data;
 namespace Restful_MagicVillaAPI.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240117074412_SeedVillaTable")]
+    partial class SeedVillaTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
